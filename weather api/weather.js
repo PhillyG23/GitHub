22ad58378f07e922025560947fc3f36f15a96c9d
Philip Delacruz <gregpdelacruz0820@gmail.com>
2023-01-29 16:24:35 -0500

    weather api

---
+++ weather api/weather.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("form");
+const cityName = document.querySelector("#city-name");
+const temperature = document.querySelector("#temperature");
+const conditions = document.querySelector("#conditions");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const city = document.querySelector("#city").value;
+  cityName.textContent = city;
+  
+  // Replace the API key and URL with your own
+  const URL = `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`;
+  
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    
+    temperature.textContent = `${Math.round(data.main.temp - 273.15)}°C`;
+    conditions.textContent = data.weather[0].description;
+  } catch (error) {
+    console.error(error);
+  }
+});
