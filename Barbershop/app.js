6db266e14f5f8d125409d50d4e3d691f32abe2aa
Philip Delacruz <gregpdelacruz0820@gmail.com>
2023-02-06 10:55:50 -0500

    added the enqueue to each of the id

--- Barbershop/app.js
+++ Barbershop/app.js
@@ -1,78 +1,65 @@
-document.addEventListener("DOMContentLoaded", function() {
-    class Queue {
-    constructor(barberName) {
-    this.barberName = barberName;
-    this.items = [];
+window.onload = function() {
+    const barbers = [
+      {
+        id: "queue1",
+        enqueueId: "enqueue1",
+        dequeueId: "dequeue1",
+        clearId: "clear1"
+      },
+      {
+        id: "queue2",
+        enqueueId: "enqueue2",
+        dequeueId: "dequeue2",
+        clearId: "clear2"
+      },
+      {
+        id: "queue3",
+        enqueueId: "enqueue3",
+        dequeueId: "dequeue3",
+        clearId: "clear3"
+      }
+    ];
+  
+    barbers.forEach((barber) => {
+      document
+        .getElementById(barber.enqueueId)
+        .addEventListener("click", () => addToQueue(barber.id));
+  
+      document
+        .getElementById(barber.dequeueId)
+        .addEventListener("click", () => removeFromQueue(barber.id));
+  
+      document
+        .getElementById(barber.clearId)
+        .addEventListener("click", () => clearQueue(barber.id));
+    });
+  
+    function addToQueue(queueId) {
+      const input = document.getElementById(queueId.replace("queue", "input"));
+      const name = input.value;
+  
+      if (!name) {
+        alert("Please enter a name.");
+        return;
+      }
+  
+      const queue = document.getElementById(queueId);
+      const li = document.createElement("li");
+      li.textContent = name;
+      queue.appendChild(li);
+      input.value = "";
     }
   
-    enqueue(element) {
-        this.items.push(element);
+    function removeFromQueue(queueId) {
+      const queue = document.getElementById(queueId);
+      queue.removeChild(queue.firstChild);
+    }
+  
+    function clearQueue(queueId) {
+      const queue = document.getElementById(queueId);
+      while (queue.firstChild) {
+        queue.removeChild(queue.firstChild);
       }
-      
-      dequeue() {
-        if (this.isEmpty())
-          return "Underflow";
-        return this.items.shift();
-      }
-      
-      front() {
-        if (this.isEmpty())
-          return "No elements in Queue";
-        return this.items[0];
-      }
-      
-      isEmpty() {
-        return this.items.length == 0;
-      }
-      
-      getQueue() {
-        return this.items;
-      }
-  }
+    }
+  };
   
-  const philip = new Queue("Philip");
-  const le = new Queue("Le");
-  const anh = new Queue("Anh");
-  
-  const allQueues = [philip, le, anh];
-  
-  document.getElementById("add").addEventListener("click", function() {
-    const customer = document.getElementById("input").value;
-    const selectedBarber = document.querySelector('input[name="barber"]:checked').value;
-  
-    allQueues.forEach((queue) => {
-      if (queue.barberName === selectedBarber) {
-        queue.enqueue(customer);
-      }
-    });
-  
-    displayQueues();
-    document.getElementById("input").value = "";
-  });
-  
-  document.getElementById("remove").addEventListener("click", function() {
-    const selectedBarber = document.querySelector('input[name="barber"]:checked').value;
-  
-    allQueues.forEach((queue) => {
-      if (queue.barberName === selectedBarber) {
-        queue.dequeue();
-      }
-    });
-  
-    displayQueues();
-  });
-  
-  function displayQueues() {
-    let output = "";
-    allQueues.forEach((queue) => {
-      output += `<h2>${queue.barberName}</h2>`;
-      output += `<ul>`;
-      queue.getQueue().forEach((customer) => {
-        output += `<li>${customer}</li>`;
-      });
-      output += `</ul>`;
-    });
-    document.getElementById("output").innerHTML = output;
-  }
-  
-});
