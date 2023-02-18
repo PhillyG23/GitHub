ab3443380543513178c12b0bdab13e81e24e0bd0
Philip Delacruz <gregpdelacruz0820@gmail.com>
2023-02-18 12:07:59 -0500

    finish add the use effect and state hooks for book

--- Crud-App-Books/backend/index.js
+++ Crud-App-Books/backend/index.js
@@ -1,7 +1,9 @@
-import express from "express"
-import mysql from "mysql"
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
 
 const app = express();
+
 
 const db = mysql.createConnection({
     host:"localhost",
@@ -15,6 +17,7 @@ const db = mysql.createConnection({
 //enter the query execution cmd
 
 app.use(express.json())
+app.use(cors())
 
 
 
--- Crud-App-Books/client/src/pages/Books.jsx
+++ Crud-App-Books/client/src/pages/Books.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios'; // import axios to make HTTP requests
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Books = () => {
-  return (
-    <div>Books</div>
-  )
-}
+  const [books, setBooks] = useState([]);
 
-export default Books
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        // Make an HTTP GET request to fetch all books
+        const res = await axios.get('http://localhost:8800/books');
+        setBooks(res.data); // Update the state with the fetched data
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchAllBooks(); // Call the function to fetch books
+  }, []); // useEffect runs only once because of the empty dependency array
+
+  return (
+    <div>
+      <h1>Phill's books</h1>
+      {/* Render the books fetched from the server */}
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+      <button><Link to="/add">Add new book </Link></button>
+    </div>
+  );
+};
+
+export default Books;
